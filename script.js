7e071ec13a2b0054ac931cb2fa1c7b1c417801f1
Chris Gramme <cgramme36@gmail.com>
2015-04-02 18:36:43 -0400

    codeodynumber1

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+
+$(document).ready(function(){
+
+
+	function loadXMLDoc(url, elementId){
+		var xmlhttp;
+		if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+ 			 xmlhttp=new XMLHttpRequest();
+ 		}else{// code for IE6, IE5
+ 			 xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+ 		}
+		xmlhttp.onreadystatechange=function(){
+  			if (xmlhttp.readyState==4 && xmlhttp.status==200){
+    				document.getElementById(elementId).innerHTML=xmlhttp.responseText;
+   			 }
+  		}
+		xmlhttp.open("GET",url,true);
+		xmlhttp.send();
+	}
+
+	$('button').click(function() {
+  	loadXMLDoc("htmltutorial1.txt","change-content");
+	});
+
+
+});
