d40d05e217972b518b045abba2872fac2b1a1c41
Chris Gramme <cgramme36@gmail.com>
2015-04-02 21:28:26 -0400

    sg

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ function loadXMLDoc(url, elementId){
   			if (xmlhttp.readyState==4 && xmlhttp.status==200){
     				document.getElementById(elementId).innerHTML=xmlhttp.responseText;
     				lessonNumber++;
+   			 }else if (xmlhttp.status==404){
+   			 		alert("Lesson not made yet, check back at a later time.");
    			 }
   		}
 		xmlhttp.open("GET",url,true);
@@ -27,6 +29,13 @@ function loadXMLDoc(url, elementId){
 
 $('#next-lesson').click(function() {
   	loadXMLDoc("htmllesson"+lessonNumber+".txt","change-content");
+});
+
+$('ul li').on('click', function() {
+
+    alert($(this).index()+1);
+    loadXMLDoc("htmllesson"+($(this).index()+1)+".txt","change-content");
+
 });
 
 function updateLessonNumber(){
