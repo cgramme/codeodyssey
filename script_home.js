658bbd13d2d82b7416ed99dccb455a1206bb64b1
Chris Gramme <cgramme36@gmail.com>
2015-04-04 05:54:46 -0400

    refs

---
+++ script_home.js
@@ -0,0 +1,62 @@
+
+
+
+$(document).ready(function(){
+	
+	$('.html-basics').mouseover(function() {
+		$('.html-basics').find('p').fadeOut(200, function(){
+			$(this).fadeIn(200).html("Learn basic knowledge of the foundational web language.");
+		});
+	}).mouseout(function() {
+		$(this).find('p').fadeOut(200, function(){
+			$(this).fadeIn(200).html("HTML Basics");
+		});
+    });
+
+    $('.html-intermediate').mouseover(function() {
+		$('.html-intermediate').find('p').fadeOut(200, function(){
+			$(this).fadeIn(200).html("Take your basic HTML to the next level with the HTML intermediate guide.");
+		});
+	}).mouseout(function() {
+		$(this).find('p').fadeOut(200, function(){
+			$(this).fadeIn(200).html("HTML Intermediate");
+		});
+    });
+
+    $('.css-basics').mouseover(function() {
+		$('.css-basics').find('p').fadeOut(200, function(){
+			$(this).fadeIn(200).html("Take control of you HTML with this basic CSS course.");
+		});
+	}).mouseout(function() {
+		$(this).find('p').fadeOut(200, function(){
+			$(this).fadeIn(200).html("CSS Basics");
+		});
+    });
+
+
+	slideText();
+
+});
+
+function slideText(){
+     $('.text-slide').css({"left":"1500px"}).animate({"left":"0px"}, 1500, function(){
+     	$('.text-slide').delay(5000).animate({"left":"-1500px"}, 1500, function(){
+     		$('.text-slide').find('h2').html("Are you just starting out?<br> Try our HTML Basics course.");
+     		$('.text-slide').find('h4').text("Start from where most web developers started.");
+     		$('.text-slide').css({"left":"1500px"}).animate({"left":"0px"}, 1500, function(){
+     			$('.text-slide').delay(5000).animate({"left":"-1500px"}, 1500, function(){
+     				$('.text-slide').find('h2').html("Need something more advanced?<br>Check out the CSS Intermediate course.");
+     				$('.text-slide').find('h4').text("Learn advanced techniques for styling your CSS.");
+     				$('.text-slide').css({"left":"1500px"}).animate({"left":"0px"}, 1500, function(){
+     					$('.text-slide').delay(5000).animate({"left":"-1500px"}, 1500, function(){
+		     				$('.text-slide').find('h2').html("Welcome to Code Odyssey!<br>Where your web-development journey begins.");
+		     				$('.text-slide').find('h4').text("Code Odyssey makes learning web-development easy and fun, through step by step tutorials.");
+		     				$('.text-slide').css({"left":"1500px"}).animate({"left":"0px"}, 1500, slideText());
+		     			});
+     				});
+     			});
+     			
+     		});
+     	});
+     });
+}
