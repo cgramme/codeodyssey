d03eb4dbfb8d2f5ad4ce968b3aa98925ebcf9f34
Chris Gramme <cgramme36@gmail.com>
2015-08-06 21:21:02 -0400

    kug

--- script_home.js
+++ script_home.js
@@ -1,50 +1,33 @@
-
 $(window).load(function(){
 	showText("#msg", "<Code Odessey/>", 0, 100);
+	listSetup();
 });
 $(document).ready(function(){
-	$('.menu .courses').mouseenter(function(){
-		listOut($('.menu li'), 0, 50);
-		listIn($('.sub-menu li'), 0, 50);
-	});
-	$('.sub-menu').mouseleave(function(){
-		listIn($('.menu li'), 0, 50);
-		listOut($('.sub-menu li'), 0, 50);
-	});
-	$('.menu-button').on('click', function(){
-		$('.menu').slideToggle(200);
-	});
-	//Course buttons below
+	$('.menu .courses').on('click',function(){listOut($('.menu li'), 0, 50);listIn($('.course-menu li'), 0, 50);});
+	$('.menu .sign-in').on('click',function(){listOut($('.menu li'), 0, 50);listIn($('.signin-menu li'), 0, 50);});
+	$('.course-menu').on('click mouseleave', function(){listIn($('.menu li'), 0, 50);listOut($('.course-menu li'), 0, 50);});
+	$('.signin-menu').on('click mouseleave', function(){listIn($('.menu li'), 0, 50);listOut($('.signin-menu li'), 0, 50);});
+	//tablet/phone size menu display
+	$('.menu-button').on('click', function(){$('.menu').slideToggle(200);});
+	//Course buttons on hover
     $('.html-basics').mouseover(function() {
 		buttonAnimate("Learn the basic principles of the foundational web language HTML.", this);
-	}).mouseout(function() {
-		buttonAnimate("HTML Basics", this);
-    });
+	}).mouseout(function() {buttonAnimate("HTML Basics", this);});
     $('.html-intermediate').mouseover(function() {
 		buttonAnimate("Take your basic knowledge of HTML to the next level with this HTML intermediate guide. Coming soon!", this);
-	}).mouseout(function() {
-		buttonAnimate("HTML Intermediate", this);
-    });
+	}).mouseout(function() {buttonAnimate("HTML Intermediate", this);});
 	$('.css-basics').mouseover(function() {
 		buttonAnimate("Add style to your HTML with this CSS basics course. Coming soon!", this);
-	}).mouseout(function() {
-		buttonAnimate("CSS Basics", this);
-    });
+	}).mouseout(function() {buttonAnimate("CSS Basics", this);});
     $('.css-intermediate').mouseover(function() {
 		buttonAnimate("Take your CSS to the next level! Coming soon!", this);
-	}).mouseout(function() {
-		buttonAnimate("CSS Intermediate", this);
-    });
+	}).mouseout(function() {buttonAnimate("CSS Intermediate", this);});
     $('.javascript-basics').mouseover(function() {
 		buttonAnimate("Take even more control of your site with Javascript Basics! Comming soon!", this);
-	}).mouseout(function() {
-		buttonAnimate("Javascript Basics", this);
-    });
+	}).mouseout(function() {buttonAnimate("Javascript Basics", this);});
     $('.sass-basics').mouseover(function() {
 		buttonAnimate("Syntactically awesome stylesheets, your css on steriods! Comming soon!", this);
-	}).mouseout(function() {
-		buttonAnimate("Sass Basics", this);
-    });
+	}).mouseout(function() {buttonAnimate("Sass Basics", this);});
 	//Start Welcome message and course recommendation sliding text
 	slideText(5000);
 });
@@ -53,37 +36,27 @@ $(document).ready(function(){
 //paramObject should be set to "this"
 function buttonAnimate(string1, paramObject){
 	$(paramObject).find('p').transition({
-			  perspective: '500px',
-			  rotateY: '-90deg',
-			  rotateX: '-90deg'
+			  perspective: '500px',rotateY: '-90deg',rotateX: '-90deg'
 			},200, function(){
 				$(paramObject).find('p').html(string1);
 				$(paramObject).find('p').transition({
-			 		 perspective: '500px',
-			  		 rotateY: '0deg',
-			  		 rotateX: '0deg'
+			 		 perspective: '500px',rotateY: '0deg',rotateX: '0deg'
 				},200);
 			}
 		);
 }
-//Welcome text and recommendations for courses animated sliding text
+//Welcome text and recommendation for courses animated sliding text
 //time variable sets pause between each message
 function slideText(time){
 	var i = 1;
 	setInterval(function(){ 
 		var listLength = $('.text-slide li').length;
-		if($('.text-slide span').length != 0){
-			$('.text-slide span').remove();
-		}
+		if($('.text-slide span').length != 0){$('.text-slide span').remove();}
 		$('.text-slide li:nth-child('+i+')').css({"display":"block"}).addClass('translate-x-in').delay(time).queue(function(next){
-						$(this).css({"display":"none"});
-						next();
-					});
-		if(i>=listLength){
-			i=1;
-		}else{
-			i++;
-		}	
+				$(this).css({"display":"none"});
+				next();
+			});
+		if(i>=listLength){i=1;}else{i++;}	
 	}, time);
 }
 //display one string text character at a time
@@ -99,19 +72,28 @@ function showText (target, message, index, interval) {
     }, interval);
   }
 }
+//listOut will remove list from view by moving item down one at a time
 function listOut (list, index, interval) {
 	if(index < list.length) {
-    $(list[index++]).css({"top":"100px"});
+    $(list[index++]).removeClass('list-in-anima').addClass('list-out-anima').hide();
     setTimeout(function () {
         listOut(list, index, interval); 
     }, interval);
   }
 }
+//listIn will show each list item one at a time by moving to original position
 function listIn (list, index, interval) {
 	if(index < list.length) {
-    $(list[index++]).css({"top":"0px"});
+    $(list[index++]).removeClass('list-out-anima').addClass('list-in-anima').show();
     setTimeout(function () {
         listIn(list, index, interval);
     }, interval);
   }
+}
+//Menu list setup and animate in
+function listSetup () {
+	listOut($('.course-menu li'), 0, 0);
+	listOut($('.signin-menu li'), 0, 0);
+	listIn($('.menu li'), 0, 200);
+	setTimeout(function () {$('.course-menu, .signin-menu').css({"opacity":"1"});}, 500);
 }
