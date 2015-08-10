4b2ad503d506e7c528c7a88e90ade9708e2e0ff0
Chris Gramme <cgramme36@gmail.com>
2015-08-10 02:39:39 -0400

    i

--- script_home.js
+++ script_home.js
@@ -14,8 +14,8 @@ $(document).ready(function(){
 	$('.menu-button').on('click', function(){
 		if($('.menu, .course-menu, signin-menu').is(":visible")){
 			listOut($('.menu li'), 0, 50);
-			listOut($('.signin-menu li'), 0, 50);});
-			listOut($('.course-menu li'), 0, 50);});
+			listOut($('.signin-menu li'), 0, 50);
+			listOut($('.course-menu li'), 0, 50);
 		}else{
 			listIn($('.menu li'), 0, 50);
 		}
